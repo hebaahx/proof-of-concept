4b669ca050c75ca9de88e3fdc06fac69e9e4577a
handouh1 <heba.handouka@hva.nl>
2026-06-09 19:58:45 +0200

    #3 html/css uitgebreid

--- server.js
+++ server.js
@@ -10,36 +10,46 @@ app.engine('liquid', engine.express())
 app.set('views', './views')
 app.set('view engine', 'liquid') // Vertel Express dat .liquid de standaard extensie is
 
+// De basis URL van de PokeAPI
+const pokeApi = 'https://pokeapi.co/api/v2'
+
 
 // Homepage route 
 app.get('/', async (req, res) => {
- 
-  // Stap 1: Haal een lijst van 20 Pokémon op van de PokeAPI
-  const listResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
-  const listData = await listResponse.json()
 
-  // Stap 2: Voor elke Pokémon halen we de detailpagina op
-  const pokemon = await Promise.all(
-    listData.results.map(async (p) => {
-      const detailResponse = await fetch(p.url)
-      const detail = await detailResponse.json()
+    try {
+ 
+    // Stap 1: Haal een lijst van 20 Pokémon op van de PokeAPI
+    const listResponse = await fetch(`${pokeApi}/pokemon?limit=20`)
+    const listData = await listResponse.json()
+
+    // Stap 2: Voor elke Pokémon halen we de detailpagina op
+    const pokemonList = await Promise.all(
+            listData.results.map(async (pokemon) => {
+
+            const detailResponse = await fetch(pokemon.url)
+            const detailData = await detailResponse.json()
+
 
     // Stap 3: Geef alleen de velden terug die we nodig hebben op de homepage
       return {
-        id: detail.id,
-        name: detail.name,
-        image: detail.sprites.front_default,
-        types: detail.types.map((t) => t.type.name), 
+        id: detailData.id,
+        name: detailData.name,
+        image: detailData.sprites.other['official-artwork'].front_default 
+              ?? detailData.sprites.front_default,
+        types: detailData.types.map((type) => type.type.name), 
       }
     })
   )  
 
-    // Stap 4: Render de homepage template en geef de pokémon data mee
-  res.render('index', { pokemon })
+// Stap 4: Render de homepage template en geef de pokémon data mee
+  res.render('index', { pokemonList })
+
+} catch (error) {
+    console.error(error)
+    res.status(500).send('something went wrong')
+   }
 })
-
-
-
 
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
